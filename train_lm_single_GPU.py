b5119aa88b92145313b91da1ec44f6aec99cdd51
leanhhoang2806 <58996802+leanhhoang2806@users.noreply.github.com>
2024-02-06 17:50:35 -0600

    running

--- train_lm_single_GPU.py
+++ train_lm_single_GPU.py
@@ -198,6 +198,9 @@
 # =========================================
 import numpy as np
 import tensorflow as tf
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
 
 # Sample training data
 X_train = np.array([
